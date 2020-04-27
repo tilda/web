dd1073d4d8f45a764d853bc02e4f21c3cfa5287b
S Stewart <tilda@users.noreply.github.com>
2020-04-27 00:55:16 -0500

    add pfp/start work on card

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="card ~neutral !normal content">
+    </section>
+</template>
+
+<style scoped>
+@import "~a17t/dist/a17t.css";
+</style>
+
+<script>
+export default {
+}
+</script>
