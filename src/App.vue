b01264b364ad37b213abd02a987949829b96976e
S Stewart <tilda@users.noreply.github.com>
2020-06-29 05:42:51 -0500

    scunthorpe problem

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  components: Header  
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="flex items-center flex-wrap justify-between">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+    </header>
+</template>
