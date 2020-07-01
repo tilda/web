f2c077cbfef73a317ba8be949ce669b1c830ebe2
S Stewart <tilda@users.noreply.github.com>
2020-07-01 06:50:46 -0500

    attempt to fix menu icon color

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,7 @@
         </div>
         <div class="block md:hidden">
           <button class="flex items-center px-3 py-3 text-gray-700 hover:text-gray-900" onclick="document.querySelector('#navButtons').classList.toggle('hidden')">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-            </svg>
+            <Unicon width="20" height="20" name="bars" :fill="iconColor"/>
           </button>
         </div> 
         <ul id="navButtons" class="align-baseline block flex-wrap md:flex md:items-center flex-grow shadow-lg py-4 px-2 md:p-0 hidden md:bg-transparent md:shadow-none">
