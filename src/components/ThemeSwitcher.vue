73802a0e961011d74d5aa417e0d69a14a8821137
S Stewart <tilda@users.noreply.github.com>
2020-07-02 04:20:20 -0500

    allow themeswitcher to actually be focused on tab

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,8 +1,8 @@
 <template>
-    <a @click="switchTheme" class="switcher align-baseline">
+    <span @click="switchTheme" class="switcher align-baseline" tabindex="0">
         <unicon v-if="this.$store.state.dark" name="sun" fill="white" aria-label="Switch to light mode"/>
         <unicon v-else name="moon" fill="black" aria-label="Switch to dark mode"/>
-    </a>
+    </span>
 </template>
 
 <script>
