7cc0d12a716edfae9db062a0f71c26b9c709bc09
S Stewart <tilda@users.noreply.github.com>
2020-07-03 20:02:38 -0500

    persist theme state across refreshes

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
+
+const persist = new VuexPersist({
+  key: 'state',
+  storage: window.localStorage
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [
+    persist.plugin
+  ],
   state: {
     dark: true
   },
