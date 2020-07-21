5b388e1b804655b85b00c40278f0d32eadf612a4
S Stewart <tilda@users.noreply.github.com>
2020-07-21 05:32:54 -0500

    indent fix in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="home">
-    <div class="md:flex md:mt-24 mb-24 items-center justify-between">
-      <section class="md:w-6/12 items-center content-center">
-        <a class="content-center mx-auto md:mr-6 mb-6">
-            <img src="../assets/avy.png" title="Art by @ahppple (Twitter)" alt="Fan art of Rin Shima from Yuru Camp looking at viewer" class="rounded-full content-center mx-auto h-48 w-48">
-        </a>
-      </section>      
-      <section class="md:w-6/12 mb-12 mt-6 mr-6 align-baseline">
-        <h1 class="heading text-lg md:text-3xl">
-          Hi, I'm <span class="rainbow">tilda</span>.
-        </h1>
-        <h2 class="subheading text-lg md:text-xl mb-6">
-          Welcome to my home on the internet. Feel free to browse the pages, after all they're made for that.
-        </h2>
-      </section>
+    <div class="home">
+        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
+            <section class="md:w-6/12 items-center content-center">
+                <a class="content-center mx-auto md:mr-6 mb-6">
+                    <img src="../assets/avy.png" title="Art by @ahppple (Twitter)" alt="Fan art of Rin Shima from Yuru Camp looking at viewer" class="rounded-full content-center mx-auto h-48 w-48">
+                </a>
+            </section>      
+            <section class="md:w-6/12 mb-12 mt-6 mr-6 align-baseline">
+                <h1 class="heading text-lg md:text-3xl">
+                    Hi, I'm <span class="rainbow">tilda</span>.
+                </h1>
+                <h2 class="subheading text-lg md:text-xl mb-6">
+                    Welcome to my home on the internet. Feel free to browse the pages, after all they're made for that.
+                </h2>
+            </section>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  components: {
-  }
+    name: 'Home',
+    components: {
+    }
 }
 </script>
