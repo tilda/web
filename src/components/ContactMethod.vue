6fc62acfa893849d89d4b6e46624dbed5b3d5018
S Stewart <tilda@users.noreply.github.com>
2020-11-02 02:34:16 -0600

    add rudimentary contact method component

---
+++ src/components/ContactMethod.vue
@@ -0,0 +1,20 @@
+<template>
+    <a href="{{ method.url }}">
+        <div class="max-w-md" style="background-color: {{ method.color }}">
+            <div class="ml-8">
+                <Unicon name="{{ method.icon }}" fill="white" icon-style="monochrome"/>
+            </div>
+            <div class="ml-16">
+                <h3>{{ method.name }}</h3>
+                <p>{{ method.description }}</p>
+            </div>
+        </div>
+    </a>
+</template>
+<script>
+export default {
+    props: {
+        method: Object
+    }
+}
+</script>
