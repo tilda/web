d8ea9244ad30aaa62a30ed5ebfcc80222c9a0ada
S Stewart <tilda@users.noreply.github.com>
2020-11-02 14:18:44 -0600

    remove descriptions

--- src/static/contacts.js
+++ src/static/contacts.js
@@ -3,28 +3,24 @@ export default [
     {
         "name": "Code::Stats",
         "icon": "wakatime", // lol
-        "description": "Where my coding statistics come from.",
         "color": "#000000",
         "url": "https://codestats.net/users/tda"
     },
     {
         "name": "Discord",
         "icon": "discord",
-        "description": "Where I spend most of my time",
         "color": "#7298da",
         "url": "javascript:void(0)"
     },
     {
         "name": "GitHub",
         "icon": "github",
-        "description": "Where I develop things sometimes.",
         "color": "#181717",
         "url": "https://github.com/tilda"
     },
     {
         "name": "GitLab",
         "icon": "gitlab",
-        "description": "I barely use this now, but it exists.",
         "color": "#fca121",
         "url": "https://gitlab.com/wontfix"
     },
@@ -32,7 +28,6 @@ export default [
     {
         "name": "Twitter",
         "icon": "twitter",
-        "description": "Thoughts of varying quality are posted here",
         "color": "#1da1f2",
         "url": "https://twitter.com/imtilda"
     }
