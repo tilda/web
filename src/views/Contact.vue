21d72a8be8d5ad09992eb4f2f4e4ee5ddd2f1560
S Stewart <tilda@users.noreply.github.com>
2020-11-02 16:10:10 -0600

    add contact page for future usage

---
+++ src/views/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
+            <section class="md:w-6/12 items-center content-center">
+                <h1 class="heading text-lg md:text-3xl">
+                    Where I am online
+                </h1>
+                <div v-for="method in this.methods" :key="[method]">
+                    <ContactMethod :method="method"/>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+import ContactMethod from '../components/ContactMethod'
+import contacts from '../static/contacts'
+
+export default {
+    data() {
+        return {
+            methods: null
+        }
+    },
+    components: {
+        ContactMethod
+    },
+    mounted() {
+        this.methods = contacts
+    }    
+}
+</script>
