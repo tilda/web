3fcadcacb4b2083d648d794631587373a6fad7ee
S Stewart <tilda@users.noreply.github.com>
2020-12-04 04:39:20 -0600

    add codestats to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,8 @@
         <div class="md:flex md:mt-24 mb-24 items-center justify-between">
             <section class="md:w-6/12 items-center content-center">
                 <h1 class="heading text-lg md:text-3xl">"What programming languages do you use?"</h1>
-                <h2 class="subheading text-lg md:text-xl mb-6">Your answer's on the right:</h2>
+                <h2 class="subheading text-md md:text-xl mb-6">Your answer's on the right:</h2>
+                <a class="text-sm" href="https://codestats.net/users/tda">(Here's where this data comes from.)</a>
             </section>      
             <section class="md:w-6/12 mb-12 mt-6 mr-6 align-baseline">
                 <CodeStats/>
