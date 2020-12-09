932df54cb26290788caf686e90f65a27376fff1c
S Stewart <tilda@users.noreply.github.com>
2020-12-09 07:56:03 -0600

    tweak margins, add _blank to codestats link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about">
-        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
-            <section class="md:w-full mb-12 mt-6 mr-6">
+        <div class="md:flex md:mt-24 mb-12 items-center justify-between">
+            <section class="md:w-full mb-4 mt-6 mr-6">
                 <h1 class="heading text-lg md:text-3xl">
                     Hi, I'm <span class="rainbow">tilda</span>.
                 </h1>
@@ -10,11 +10,11 @@
                 </h2>
             </section>
         </div>
-        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
+        <div class="md:flex md:mt-4 mb-24 items-center justify-between">
             <section class="md:w-6/12 items-center content-center">
                 <h1 class="heading text-lg md:text-3xl">"What programming languages do you use?"</h1>
                 <h2 class="subheading text-md md:text-xl mb-6">Your answer's on the right:</h2>
-                <a class="text-sm" href="https://codestats.net/users/tda">(Here's where this data comes from.)</a>
+                <a class="text-sm" target="_blank" href="https://codestats.net/users/tda">(Here's where this data comes from.)</a>
             </section>      
             <section class="md:w-6/12 mb-12 mt-6 mr-6 align-baseline">
                 <CodeStats/>
