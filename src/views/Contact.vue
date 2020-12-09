bbd618cb192cd09f82425a0f0fb76b65997ae201
S Stewart <tilda@users.noreply.github.com>
2020-12-09 08:24:23 -0600

    idk things here i guess

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
-            <section class="md:w-6/12 items-center content-center">
+        <div class="md:flex md:mt-24 mb-24">
+            <section class="md:w-full mb-12 mt-6 mr-6">
                 <h1 class="heading text-lg md:text-3xl">
                     Where I am online
                 </h1>
+                <h2 class="subheading text-md md:text-xl mb-6">
+                    If you need to get ahold of me, here's how you do it.
+                </h2>
                 <div v-for="method in this.methods" :key="[method]">
                     <ContactMethod :method="method"/>
                 </div>
