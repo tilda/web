1fdf04f671149bf41a751d8cace80eb1675080a3
S Stewart <tilda@users.noreply.github.com>
2020-12-09 08:23:31 -0600

    switch footer to mx-auto

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <span class="text-sm md:text-center">
+        <span class="text-sm mx-auto">
             color scheme: <a href="https://nordtheme.com">nord</a> - made using <a href="https://vuejs.org">vue.js</a>
             <br/>&hearts;
         </span>
