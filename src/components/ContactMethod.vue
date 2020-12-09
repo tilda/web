3d078ef9dea0cd174b6622cb949d999fe64d9392
S Stewart <tilda@users.noreply.github.com>
2020-12-09 07:11:28 -0600

    almost done with ContactMethod

--- src/components/ContactMethod.vue
+++ src/components/ContactMethod.vue
@@ -1,30 +1,27 @@
 <template>
     <div>
-        <a :href="method.url" target="_blank" class="text-decoration-important block">
-            <div class="flex max-w-lg rounded shadow-lg hover:shadow-xl vertical-align-important" :style="{ 'background-color': method.color }">
-                <div class="mx-4 p-4 border-r-4 border-white">
-                    <svg role="img" class="h-12 w-12" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+        <a :href="method.url" target="_blank" class="text-decoration-important max-w-xs">
+            <div class="flex max-w-xs rounded shadow-md hover:shadow-lg">
+                <div class="mr-4 p-4 border-r-4 border-transparent" :style=" { 'background-color': method.color }">
+                    <svg role="img" class="h-8 w-8" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
                         <title>{{ method.name }} icon</title>
                         <path :d="getIcon(method.icon)"/>
                     </svg>
                 </div>
-                <div class="ml-8 vertical-align-important text-xl">
+                <span class="text-xl text-center">
                     {{ method.name }}
-                </div>
+                </span>
             </div>
         </a>
     </div>
 </template>
 <style scoped>
-    .vertical-align-important {
-        align-items: baseline !important;
-    }
     .text-decoration-important {
         text-decoration: none !important;
     }
 </style>
 <script>
-import * as icons from 'simple-icons'
+import icons from 'simple-icons'
 
 export default {
     props: {
