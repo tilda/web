b619b1670092c7983cb81a6b117785789b85a7bf
tilda <iamtheworst@programmer.net>
2021-05-14 16:48:51 -0500

    make the home blurb actually readable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="home">
-        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
-            <section class="md:w-6/12 items-center content-center">
+        <div class="md:flex md:mt-16 mb-8 items-center justify-between">
+            <section class="md:w-full">
                 <a class="content-center mx-auto md:mr-6 mb-6">
                     <img src="../assets/avy.png" title="Art by @ahppple (Twitter)" alt="Fan art of Rin Shima from Yuru Camp looking at viewer" class="rounded-full content-center mx-auto h-48 w-48">
                 </a>
-            </section>      
-            <section class="md:w-6/12 mb-12 mt-6 mr-6 align-baseline">
+            </section>
+        </div>
+        <div class="md:flex md:mt-8 mb-24 items-center justify-between">
+            <section class="md:w-full mb-8 mt-6">
                 <h1 class="heading text-lg md:text-3xl">
                     Hi, I'm <span class="rainbow">tilda</span>.
                 </h1>
-                <h2 class="subheading text-lg md:text-xl mb-6">
-                    Welcome to my home on the internet. Feel free to browse the pages, after all they're made for that.
+                <h2 class="subheading text-lg md:text-xl">
+                    Welcome to my home on the internet.<br/>
+                    Feel free to browse the pages,<br/>
+                    after all they're made for that.
                 </h2>
             </section>
         </div>
