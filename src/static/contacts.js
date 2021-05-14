a56e0d5309feb26d096bcab7c1441fdd965e78d8
tilda <iamtheworst@programmer.net>
2021-05-14 14:20:52 -0500

    redo contacts.js format

--- src/static/contacts.js
+++ src/static/contacts.js
@@ -1,22 +1,20 @@
 /* eslint-disable no-unused-vars */
-export default [
+const INFO = [
     {
-        "name": "GitHub",
         "icon": "github",
-        "color": "#181717",
         "url": "https://github.com/tilda"
     },
     {
-        "name": "GitLab",
         "icon": "gitlab",
-        "color": "#fca121",
         "url": "https://gitlab.com/wontfix"
     },
 
     {
-        "name": "Twitter",
         "icon": "twitter",
-        "color": "#1da1f2",
         "url": "https://twitter.com/imtilda"
     }
 ]
+
+export default INFO.sort(function(a, b) {
+    return a.length - b.length
+})
