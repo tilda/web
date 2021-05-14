b40c742a8289d78692a519730ca69d87f254a167
tilda <iamtheworst@programmer.net>
2021-05-14 17:32:46 -0500

    we do a little bit of FUCKING trolling

--- src/components/ContactMethod.vue
+++ src/components/ContactMethod.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <a :href="method.url" target="_blank" :title="icon.title" class="mb-4 mx-auto rounded shadow-md hover:shadow-lg contacts">
-                <div class="p-4" :style=" { 'background-color': '#' + icon.hex }">
+        <a :href="method.url" target="_blank" :title="icon.title" class="mb-4 mx-auto rounded contacts">
+                <div class="p-4 shadow-m hover:shadow-lg" :style=" { 'background-color': '#' + icon.hex }">
                     <svg role="img" class="h-8 w-8" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
                         <path :d="icon.path"/>
                     </svg>
