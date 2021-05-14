2272334f695973536678e131a8c2f35c609ac956
tilda <iamtheworst@programmer.net>
2021-05-14 15:42:50 -0500

    remove nord link from footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <span class="text-sm mx-auto">
-            color scheme: <a href="https://nordtheme.com">nord</a> - made using <a href="https://vuejs.org">vue.js</a>
+            made using <a href="https://vuejs.org">vue.js</a>
             <br/>&hearts;
         </span>
     </footer>
