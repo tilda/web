017f5e35b0ee9421bbd1f50e8c7166e29761b577
tilda <iamtheworst@programmer.net>
2021-06-05 17:29:00 -0500

    move social media to about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,34 +1,39 @@
 <template>
     <div class="about">
         <div class="md:flex md:mt-24 mb-12 items-center justify-between">
-            <section class="md:w-full mb-4 mt-6 mr-6">
+            <section class="md:w-full mb-4">
                 <h1 class="heading text-lg md:text-3xl">
                     Hi, I'm <span class="rainbow">tilda</span>.
                 </h1>
                 <h2 class="subheading text-lg md:text-xl mb-6">
                     I do stuff on the internet sometimes.
                 </h2>
+                <div class="md:flex flex-row items-center justify-center space-x-3">
+                    <span v-for="method in this.methods" :key="[method]">
+                        <ContactMethod :method="method"/>
+                    </span>
+                </div> 
             </section>
         </div>
-        <div class="md:flex md:mt-24 mb-24 items-center justify-between">
+        <div class="md:flex md:mt-12 mb-12 items-center justify-between">
             <section class="md:w-full mb-4 mt-6 mr-6">
                 <h1 class="heading text-lg md:text-3xl">A couple of facts about myself!</h1>
                 <h2 class="subheading text-md md:text-xl mb-6">(I mean if you're here, you might as well get to know me a bit)</h2>
                 <ul>
-                    <li>My pronouns are he/him (or they/them)!</li>
+                    <li>I'm a he (or a they). Either is fine :)</li>
                     <li>I'm pretty into computers, they're fun. (most of the time)</li>
                     <li>I like trying to help people when they have issues.</li>
                     <li>Talking your brain out with subjects you have no interest in is my specialty~</li>
                 </ul>
             </section>
         </div>
-        <div class="md:flex md:mt-4 mb-24 items-center justify-between">
+        <div class="md:flex mb-24 items-center justify-between">
             <section class="md:w-6/12 items-center content-center">
                 <h1 class="heading text-lg md:text-3xl">I program in these languages &#8594;</h1>
-                <h2 class="subheading text-md md:text-xl mb-6">(Though, could you consider Markdown anything near programming?!)</h2>
+                <h2 class="subheading text-md md:text-xl mb-6">(Though, could you consider Markdown anything near programming?)</h2>
                 <a class="text-sm" target="_blank" href="https://codestats.net/users/tda">(Here's where this data comes from.)</a>
             </section>      
-            <section class="md:w-6/12 mb-12 mt-6 mr-6 align-baseline">
+            <section class="md:w-6/12 mt-6 mr-6 align-baseline">
                 <CodeStats/>
             </section>
         </div>
@@ -37,10 +42,18 @@
 
 <script>
 import CodeStats from '../components/CodeStats'
+import ContactMethod from '../components/ContactMethod.vue'
+import contacts from '../static/contacts'
 
 export default {
+    data() {
+        return {
+            methods: contacts
+        }
+    },
     components: {
         CodeStats,
+        ContactMethod,
     }  
 }
 </script>
