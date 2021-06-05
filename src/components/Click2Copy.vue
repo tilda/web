16d989d4e78af39c48efb6cf85c7d87128a32117
tilda <iamtheworst@programmer.net>
2021-06-05 17:28:15 -0500

    add email/discord on contact page

---
+++ src/components/Click2Copy.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mb-2">
+        <input id="aaghh" class="rainbow text-xl md:text-4xl" type="text" @click="copy" :value="text" readonly>
+        <br/>
+        <small id="affirm">Click to copy!</small>
+    </div>
+</template>
+
+<style scoped>
+    #aaghh {
+        background-color: transparent;
+        text-align: center;
+        cursor: pointer;
+    }
+</style>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        copy: function() {
+            const the = document.getElementById('aaghh')
+            const p = document.getElementById('affirm')
+            the.select()
+            the.setSelectionRange(0, 99999) // the joys of mobile!
+            document.execCommand("copy")
+            p.innerHTML = 'Copied!'
+            setTimeout(function() {
+                p.innerHTML = 'Click to copy!'
+            }, 1500)
+        }
+    }
+}
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,32 +1,33 @@
 <template>
     <div>
-        <div class="md:flex md:mt-24">
-            <section class="md:w-full mb-4 mt-6 mr-6">
-                <h1 class="heading text-lg md:text-3xl">
-                    Where I am online
+        <div class="md:flex">
+            <section class="md:w-full md:max-h-screen mb-4">
+                <h1 class="heading text-lg md:text-2xl mb-2">
+                    Contact via email:
                 </h1>
+                <Click2Copy text="tda@stairway.cf"/>
+                <h1 class="heading text-lg md:text-2xl mb-2">
+                    Or contact via Discord:
+                </h1>
+                <Click2Copy text="tilda#6729"/>
+                <h1 class="heading text-lg md:text-2xl mb-2">
+                    Also see the <router-link to="/about">About</router-link> page for social media accounts!
+                </h1>
+                <h2 class="subheading mt-2">
+                    As long as it's not spam, I'll probably respond.
+                </h2>
             </section>
         </div>
-        <div class="md:flex flex-row mb-12 items-center justify-center space-x-3">
-            <span v-for="method in this.methods" :key="[method]">
-                <ContactMethod :method="method"/>
-            </span>
-        </div> 
+
     </div>
 </template>
 
 <script>
-import ContactMethod from '../components/ContactMethod'
-import contacts from '../static/contacts'
+import Click2Copy from '../components/Click2Copy.vue'
 
 export default {
-    data() {
-        return {
-            methods: contacts
-        }
-    },
     components: {
-        ContactMethod
+        Click2Copy
     },
 }
 </script>
