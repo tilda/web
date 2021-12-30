56c38cfa6e83ef9c8efe8df61a768ee21b47172d
tilda <iamtheworst@programmer.net>
2021-12-30 02:41:59 -0600

    fix weird contactmethod rendering on lowres

--- src/components/ContactMethod.vue
+++ src/components/ContactMethod.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <a :href="method.url" target="_blank" :title="icon.title" class="mb-4 mx-auto rounded contacts">
-                <div class="p-4 shadow-m hover:shadow-lg" :style=" { 'background-color': '#' + icon.hex }">
+                <div class="p-4 shadow-m hover:shadow-lg flex flex-row" :style=" { 'background-color': '#' + icon.hex }">
                     <svg role="img" class="h-8 w-8" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
                         <path :d="icon.path"/>
                     </svg>
+                    <span class="md:hidden text-xl pl-4">{{ icon.title }}</span>
                 </div>
         </a>
     </div>
