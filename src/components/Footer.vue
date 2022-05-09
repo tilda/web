b5498464a1966e9bc38e00eb9991c8b65057a784
tilda <iamtheworst@programmer.net>
2022-05-09 05:52:05 -0500

    i can't make up my mind on this damn footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,7 @@
     <footer class="mt-12">
         <span class="text-sm mx-auto">
             thanks for visiting! &hearts;
+            <br/><span class="text-xs">made using <a href="https://vuejs.org">vue.js</a> & <a href="https://tailwindcss.com">tailwindcss</a></span>
         </span>
     </footer>
 </template>
