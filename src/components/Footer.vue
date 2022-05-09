8d2e99a00783526750e9a01dd9d696465634c628
tilda <iamtheworst@programmer.net>
2022-05-09 04:39:28 -0500

    e

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="mt-12">
-        <span class="text-xs mx-auto">
+        <span class="text-sm mx-auto">
             thanks for visiting! &hearts;
         </span>
     </footer>
