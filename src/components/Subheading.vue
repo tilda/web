42b0585012b52c0926b78572bda22b111d708c68
tilda <iamtheworst@programmer.net>
2022-05-11 05:42:17 -0500

    fix subheading

--- src/components/Subheading.vue
+++ src/components/Subheading.vue
@@ -3,6 +3,11 @@
         <slot></slot>
     </h2>
 </template>
+<style scoped>
+    .text-sm {
+        font-size: 0.875rem !important;
+    }
+</style>
 <script>
 export default {
     data() {
@@ -28,7 +33,8 @@ export default {
         }
 
         if (!this.bold) {
-            // By sheer luck, the subheading class enforces bold font, so we can get away // with just 1 branch on this
+            // By sheer luck, the subheading class enforces bold font,
+            // so we can get away with just 1 branch on this
             this.classList.push('font-normal')
         }
     }
