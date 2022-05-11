4f3ba269d75da825c3cf120c5eed30917b225416
tilda <iamtheworst@programmer.net>
2022-05-11 07:22:08 -0500

    fix buttercms attribution on light theme

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,15 @@
             thanks for visiting! &hearts;
             <br/>made using <a href="https://vuejs.org">vue.js</a> & <a href="https://tailwindcss.com">tailwindcss</a>
         </span>
-        <br/><span class="text-xs mx-auto">blog engine provided by <a href="https://buttercms.com" target="_blank"><img src="https://cdn.buttercms.com/RyJ7UhcVTCRqrCFXwgCo" alt="ButterCMS" style="display: inline;" width="80" height="30"></a></span>
+        <br/><span class="text-xs mx-auto">blog engine provided by <a href="https://buttercms.com" target="_blank"><img :src="butterCmsLogo" alt="ButterCMS" style="display: inline;" width="80" height="30"></a></span>
     </footer>
 </template>
+<script>
+export default {
+    computed: {
+        butterCmsLogo: function() {
+            return (this.$store.state.dark ? 'https://cdn.buttercms.com/RyJ7UhcVTCRqrCFXwgCo' : 'https://cdn.buttercms.com/PGJPyIwaQ2KnOA8UyKfH')
+        }
+    }
+}
+</script>
