b29968c08b5c3b4ad9e0bce5cd057f5035f36907
tilda <iamtheworst@programmer.net>
2022-05-13 13:19:20 -0500

    exclude unused icons from build (closes #13)

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const ctxReplacement = require('webpack').ContextReplacementPlugin
+const contacts = import('./src/static/contacts.mjs')
+
+function getAllUsedIcons() {
+    let icons = []
+    for (let socials; socials in contacts;) {
+        icons.push(socials.icon)
+    }
+    return icons.join('|')
+}
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new ctxReplacement(
+                /simple-icons[/\\]icons$/,
+                new RegExp(`/${getAllUsedIcons()}/`)
+            )
+        ]
+    }
+}
