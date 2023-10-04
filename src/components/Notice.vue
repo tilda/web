09e8824b2dba48ad68bae757e5ad52c136023502
tilda <iamtheworst@programmer.net>
2023-10-03 19:14:42 -0500

    you can stop complaining now

---
+++ src/components/Notice.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="rounded-lg">
+        <div class="ml-4">
+            <span>{{ this.importanceEmoji }}</span>
+            <span>{{ message }}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        importance: {
+            type: String,
+            default: 'info',
+            validator: function(level) {
+                return (['error', 'info', 'warning'].includes(level))
+            }
+        },
+        message: {
+            type: String,
+            required: true
+        }
+    },
+    mounted() {
+        switch (this.importance) {
+            case 'error':
+                this.importanceEmoji = '❌'
+                break
+            case 'info':
+                this.importanceEmoji = 'ℹ️'
+                break
+            case 'warning':
+                this.importanceEmoji = '⚠️'
+                break
+        }
+    }
+}
+</script>
