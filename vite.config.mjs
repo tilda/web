13ed1c3a54c6fe4b08d213c147fa3ff3ae89e740
tilda <git@til.pm>
2025-08-02 00:36:46 -0500

    add `vite-plugin-vercel`, intending to deprecate previous cfworkers-based function

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from 'vite-plugin-vercel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), tailwindcss()],
+    plugins: [vue(), tailwindcss(), vercel()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
