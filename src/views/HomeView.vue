78e497f0fbb68b8d660067447ec29cdf5f30db9d
tilda <git@til.pm>
2026-01-11 17:10:51 -0600

    some rewrites

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,9 @@
 <template>
-    <div class="leading-8 text-justify">
-        <div class="border-2 border-ctp-overlay0 p-4 mb-4">
-            <h1>hi, i'm tilda!</h1>
-            <span class="italic text-sm">note: not my real name, although a lot of people tend to treat it as one.</span>
-        </div>
+    <div>
         <p>
-            i'm a student currently attending <a href="https://nctc.edu">North Central Texas College</a> for an <abbr title="Associate of Applied Science">A.A.S</abbr> in cybersecurity.
-            for as long as i can remember, computers have been my favorite thing on the planet (being basically born into them); the mission statement for my career is making a net positive change in the world using my work.<br/>
-            my current claim to fame is holding a volunteer staff position as "Beatmap Nominator" in the <a href="https://osu.ppy.sh/users/6233296">community-based rhythm game <i>osu!</i></a>, helping with official leaderboard support and other various duties.
+            i'm currently a full-time student pursuing an <abbr title="Associate of Applied Science">A.A.S</abbr> in cybersecurity.
+            for as long as i can remember, computers have been my favorite thing on the planet (being basically born into them); my career's mission statement is making a net positive change in the world using my work.<br/>
+            my current claim to fame is holding a volunteer staff position as "Beatmap Nominator" in the <a href="https://osu.ppy.sh/users/6233296">community-based rhythm game <i>osu!</i></a>, helping with official leaderboard support and other various duties.<br/>
         </p>
         <h2>programming & projects</h2>
         <ol class="list-disc">
@@ -17,13 +13,13 @@
                 the bot was discontinued in early 2022 due to Discord's platform limitations quickly catching up with the old codebase.
             </li>
             <li>
-                in more recent years i've transitioned to a primary focus on frontend work, as i found myself enjoying the design aspect much more.<br/>
-                <a href="https://github.com/tilda/web">the website you're reading right now</a> is the main example of this (seeing multiple codebase rewrites over its lifespan).
-                this current version is using Vue.js v3; previous iterations used Vue.js v2, as well as vanilla HTML & JavaScript.
+                i've transitioned to a primary focus on frontend work for personal projects, as i found myself enjoying the design aspect much more.<br/>
+                most of that experience has been with <a href="https://vuejs.org">Vue.js</a>, which i learned to appreciate the most out of most "mainstream" frameworks.
             </li>
             <li>
                 i haven't managed to get many other projects off the ground unfortunately - this is something i want to change.
-                the main project i'm currently in planning stages for is <a href="https://pcr.til.pm">PriConne Mapping</a>, but there's hopefully more on the way!
+                my personal projects tend to be very interest-driven (whatever i'm obsessed with is usually the thing i'm building for).<br/>
+                the main project i'm planning for (expanding) is <a href="https://pcr.til.pm">PriConne Mapping</a>, but there's hopefully more on the way!
             </li>
         </ol>
         <h2>other hobbies</h2>
@@ -31,11 +27,20 @@
             <li>
                 rhythm games are my current foremost favorite thing. i registered for an account on <i>osu!</i> in 2015 since "it's a free game,"
                 but eventually discovered i'm just a really big fan of what <i>osu!</i> and its ilk have to offer. that eventually lead to me discovering games such as <i>beatmania IIDX</i>
-                through the rhythm game community and its original soundtrack. <i>IIDX</i> is my current favorite <abbr title="vertical scrolling rhythm game">VSRG</abbr> even if i'm not very good at it (as of November 2025, you can put me on a lower-end <i>NORMAL</i> and i might still fail).
+                through the rhythm game community and its original soundtrack. <i>IIDX</i> is my current favorite <abbr title="vertical scrolling rhythm game">VSRG</abbr> even if i'm not very good at it
+                (as of November 2025, you can put me on a lower-end <i>NORMAL</i> and i might still fail).
             </li>
             <li>
-                this might be slightly obvious - i like anime. not in the <a href="https://knowyourmeme.com/memes/thing-japan">"thing, japan"</a> type of way, but i just think it's a great medium.
-                
+                anime has quite a strong place in my heart - although i was introduced to it much earlier, my first solo watch in late 2019 (over winter break, fitting for the show) was
+                <a href="https://https://anilist.co/anime/98444/LaidBack-Camp/"><i>Laid-Back Camp</i></a>.
+            </li>
+            <li>
+                also video games as a medium are great. i was raised on Valve's <i>Half-Life</i> and <i>Portal</i> series (as well as <i>Minecraft</i>, sometimes modded).<br/>
+                a lot of my time is currently spent with Cygames' franchises*, such as <a href="https://priconne-redive.jp/"><i>Princess Connect! Re: Dive</i> (Japanese)</a> and <a href="https://umamusume.com"><i>Umamusume: Pretty Derby</i></a>.<br/>
+                <small>
+                    *big disclaimer: gacha games have many issues; too many to state here! they are not for everyone!<br/>
+                    i set reasonable limits (time, money, and other factors) for myself when playing ^^ 
+                </small>
             </li>
         </ol>
     </div>
