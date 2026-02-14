ac7b5db2f1faac06fbf80765615d5bd7eac24661
tilda <git@til.pm>
2026-02-14 11:07:48 -0600

    port contacts data from next branch for later use

--- src/static/contacts.mjs
+++ src/static/contacts.mjs
@@ -1,3 +1,43 @@
-modules.exports = {
-    
-}
+/* eslint-disable no-unused-vars */
+import { siAnilist, siGithub, siGitlab, siLastdotfm, siOsu, siSteam, siTwitch, siTwitter, siYoutube } from 'simple-icons'
+
+export default [
+    {
+        "icon": siAnilist,
+        "url": "https://anilist.co/user/tda"
+    },
+    {
+        "icon": siGithub,
+        "url": "https://github.com/tilda"
+    },
+    {
+        "icon": siGitlab,
+        "url": "https://gitlab.com/wontfix"
+    },
+    {
+        // SIDENOTE: this is called last.fm in the object
+        // but that doesn't work when you're using require so oops
+        "icon": siLastdotfm,
+        "url": "https://last.fm/user/imtilda"
+    },
+    {
+        "icon": siOsu,
+        "url": "https://osu.ppy.sh/users/6233296"
+    },
+    {
+        "icon": siSteam,
+        "url": "https://steamcommunity.com/profiles/76561198174587907"
+    },
+    {
+        "icon": siTwitch,
+        "url": "https://twitch.tv/imtilda"
+    },
+    {
+        "icon": siTwitter,
+        "url": "https://twitter.com/imtilda"
+    },
+    {
+        "icon": siYoutube,
+        "url": "https://www.youtube.com/channel/UCI3mmMbWRHo4VI_sKDfqBaQ"
+    },
+]
