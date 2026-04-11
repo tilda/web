199ff2ae31549c63e10afad40f4e1e44c2f36b51
tilda <git@til.pm>
2026-04-11 01:38:09 -0500

    working retrieval from bear in `retrieveFromRss`

--- _api/retrieveFromRss.js
+++ _api/retrieveFromRss.js
@@ -1,46 +1,55 @@
 import { parseAtomFeed } from 'feedsmith'
 
-export const config = {
-    path: "/retrieveFromRss/:page?"
-}
-
-export default async (_req, ctx) => {
+export default async (req) => {
     try {
-        let { pageParam } = ctx.params
-        if (pageParam === null) {
-            pageParam = 1
+        const REQUEST_URL = new URL(req.url)
+        if (REQUEST_URL.searchParams.get("page")) {
+            pageParam = REQUEST_URL.searchParams.get("page")
         } else {
-            pageParam = Number.parseInt(pageParam, 10).toFixed(0)
+            pageParam = 1
         }
+
         const MAX_PAGE_SIZE = 6
 
-        const result = fetch('https://tilda.bearblog.dev/feed', {
-            headers: {
+        const result = await fetch('https://tilda.bearblog.dev/feed/', {
+            'headers': {
                 'User-Agent': 'flourite (github.com/tilda/web, mailto:me@til.pm)',
                 'Accept': 'application/atom+xml'
             }
-        }).catch((err) => {
-            throw new Error(`Caught error while requesting Bear RSS feed: ${err}`)
-        }).then((res) => {
-            return parseAtomFeed(res.text)
         })
-
-        const PAGE_LIMIT = (result.feed.items.length / MAX_PAGE_SIZE + 1).toFixed(0)
-
-        if (pageParam > PAGE_LIMIT) {
-            throw new Error(`Requested page ${pageParam} does not currently exist`)
+        if (!result.ok) {
+            throw new Error(`Caught HTTP error code ${result.status} while requesting from Bear`)
         }
+        const BEAR_RESPONSE = await result.text()
+        const feed = parseAtomFeed(BEAR_RESPONSE)
 
-        return new Response(JSON.stringify({
-            'status': 'ok',
-            'current_page': pageParam,
-            'max_pages': PAGE_LIMIT,
-            'items': result.feed.items.slice((pageParam - 1) * MAX_PAGE_SIZE, pageParam * MAX_PAGE_SIZE)
-        }))
+        if (feed.entries) {
+            const PAGE_LIMIT = Number.parseInt((feed.entries.length / MAX_PAGE_SIZE + 1).toFixed(0), 10)
+
+            if (pageParam > PAGE_LIMIT) {
+                throw new Error(`Requested page ${pageParam} does not currently exist`)
+            }
+
+            return new Response(JSON.stringify({
+                'status': 'ok',
+                'current_page': pageParam,
+                'max_pages': PAGE_LIMIT,
+                'items': feed.entries.slice((pageParam - 1) * MAX_PAGE_SIZE, pageParam * MAX_PAGE_SIZE)
+            }), {
+                'headers': {
+                    'Netlify-Vary': 'query=page',
+                    'Netlify-CDN-Cache-Control': 'public, max-age=300, stale-while-revalidate=30, durable'
+                }
+            })
+        } else {
+            return new Response(null, { status: 204 })
+        }
     } catch (err) {
+        console.log(err)
+
         return new Response(JSON.stringify({
             'status': 'err',
-            'error': err
+            'error': err.toString()
         }), {
             status: 400
         })
