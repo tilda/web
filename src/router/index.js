546a7a783971ec1af1153177cef0d6a3d03a1b27
tilda <git@til.pm>
2026-06-22 18:19:35 -0500

    change blog router desc

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
         component: BlogView,
         meta: {
             heading: "blog posts",
-            subheading: "when i want to type words, they might go here!",
+            subheading: "customary various ramblings",
             wip: true
         }
     }
