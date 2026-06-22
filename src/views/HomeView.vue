0cc074391aed51f5c252946866080cfcde15b7df
tilda <git@til.pm>
2026-06-22 18:47:27 -0500

    homepage content rewrite

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,49 @@
 <template>
     <div>
         <p>
-            i'm currently a full-time student pursuing an <abbr title="Associate of Applied Science">A.A.S</abbr> in cybersecurity.
-            for as long as i can remember, computers have been my favorite thing on the planet (being basically born into them); my career's mission statement is making a net positive change in the world using my work.<br/>
-            my current claim to fame is holding a volunteer staff position as "Beatmap Nominator" in the <a href="https://osu.ppy.sh/users/6233296">community-based rhythm game <i>osu!</i></a>, helping with official leaderboard support and other various duties.<br/>
+            welcome to my portfolio of sorts!
+            for as long as i can remember, computers have been my favorite thing on the planet (having been basically born into them); my career's mission statement is making a net positive change in the world using my work.<br/>
+            in recent years my claim to fame has been in the <a href="https://osu.ppy.sh/users/6233296">community-based rhythm game <i>osu!</i></a>, as a creator, player, and <i>Beatmap Nominator</i> (tldr: quality assurance).<br/>
         </p>
-
-        <h2>programming & projects</h2>
+        <h2>my favorite...</h2>
+        <table class="indent-2">
+            <tbody>
+                <tr>
+                    <th>rhythm games</th>
+                    <td><a href="https://osu.ppy.sh"><i>osu!</i></a>, <i>BEMANI</i> titles*</td>
+                </tr>
+                <tr>
+                    <th>story/narrative games</th>
+                    <td><i>Half-Life</i> and <i>Portal</i> series</td>
+                </tr>
+                <tr>
+                    <th>multimedia franchises**</th>
+                    <td><a href="https://priconne-redive.jp/"><i>Princess Connect!</i></a>, <a href="https://umamusume.com"><i>Umamusume</i></a></td>
+                </tr>
+                <tr>
+                    <th>anime</th>
+                    <td><a href="https://anilist.co/anime/98444"><i>Laid-Back Camp</i></a>, <a href="https://anilist.co/anime/99900"><i>Pompo: The Cinéphile</i></a></td>
+                </tr>
+            </tbody>
+        </table>
+        <p class="text-sm">
+            *: Konami's arcade rhythm games. as of May 2026, i have played <i>beatmania IIDX</i>, <i>jubeat</i>, and <i>SOUND VOLTEX</i>.
+            out of those three, <i>IIDX</i> remains my favorite.<br/>
+            notably, <i>pop'n music</i> has a new release with cabinets in the US (which is great!), but it'll be a while before i get to try it.<br/>
+            **: Cygames' gacha games that would be better if they <em>weren't</em> gacha games.
+        </p>
+        <h2>what i've shipped</h2>
         <ol class="list-disc">
             <li>
                 starting in early 2017, i wrote & ran a small Discord bot named <a href="https://github.com/lolbot-project"><span class="italic">lolbot</span></a> in Python using the discord.py library.
                 its main mission was enhancing conversations by bridging fun internet services such as cat(girl) pictures, which was provided to around 700 servers at its peak.
-                the bot was discontinued in early 2022 due to Discord's platform limitations quickly catching up with the old codebase.
+                the bot was discontinued in early 2022 due to platform requirements and limitations catching up with the old codebase.
             </li>
             <li>
-                since then, i admittedly haven't been working as hard on any other projects (as much as i'd like to, the burnout hit like a truck).<br/>
-                i also slowly transitioned to focus on frontend, mainly using <a href="https://vuejs.org">Vue.js</a> - a framework i've been enamored with ever since i discovered it.<br/>
-                my hope is that in the future, i'll be motivated enough to once again pursue and ship projects i'm confident with!
-            </li>
-        </ol>
-        <h2>other hobbies</h2>
-        <ol class="list-disc">
-            <li>
-                rhythm games are my current foremost favorite thing. i registered for an account on <a href="https://osu.ppy.sh"><i>osu!</i></a> in 2015 since "it's a free game,"
-                but also discovered i'm just a really big fan of what <i>osu!</i> had to offer.<br/>
-                eventually, i found its contemporaries such as <i>beatmania IIDX</i> through the rhythm game community and original soundtracks.<br/>
-                my current favorite <abbr title="vertical scrolling rhythm game">VSRG</abbr> is <i>IIDX</i>, even if i'm not very good at it!
-            </li>
-            <li>
-                although i was introduced to anime much earlier, my first solo watch in 2019 (over winter break, quite fitting) was
-                <a href="https://https://anilist.co/anime/98444/LaidBack-Camp/"><i>Laid-Back Camp</i></a>. this shaped my favorite genres to be "slice of life" adjacent almost immediately.<br/>
-                i do try to appreciate other genres though, for example i loved the <a href="https://anilist.co/anime/97986/Made-in-Abyss/"><i>Made in Abyss</i></a> series.
-            </li>
-            <li>
-                video games as a medium; having raised myself on Valve's <i>Half-Life</i> and <i>Portal</i> series (as well as Mojang's <i>Minecraft</i>).<br/>
-                a lot of my time is currently spent with Cygames' franchises*, such as <a href="https://priconne-redive.jp/"><i>Princess Connect! Re: Dive</i> (Japanese)</a> and <a href="https://umamusume.com"><i>Umamusume: Pretty Derby</i></a>.<br/>
-                <small>
-                    *disclaimer: gacha games have many issues; too many to state here! they are not for everyone!<br/>
-                    i set reasonable limits for myself, and suggest you do the same if playing them
-                </small>
+                since then... not much (besides this website)! i was so invested in <i>lolbot</i> at the time that its forced demise burnt me out.<br/>
+                despite all of that, i've kept a constant interest in front-end development (particularly with <a href="https://vuejs.org">vue.js</a>) and linux system administration.<br/>
+                <i>eventually there will be more to say here!</i>
             </li>
         </ol>
     </div>
 </template>
-
-
