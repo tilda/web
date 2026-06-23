f1a97fb3d5cbec4b8cbc0e8d867d6bc0a732b078
tilda <git@til.pm>
2026-06-22 23:42:58 -0500

    negative tabindex for theme icon if system theme
    also added a (system theming) marker for the alt text

--- src/components/ThemeSelector.vue
+++ src/components/ThemeSelector.vue
@@ -1,19 +1,41 @@
 <template>
     <div>
         <!-- TODO: accomodate keyboard accessibility better i really dont like this? -->
-        <div role="button" :aria-readonly="themeStore.system" :title="`Current theme: ${themeStore.current}`" tabindex="0" :class="`size-6 inline-block ${themeStore.system ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`">
-            <MoonIcon v-if="themeStore.current == 'macchiato'" @click="themeStore.setLight" @keyup.enter="themeStore.setLight"/>
-            <SunIcon v-else-if="themeStore.current == 'latte'" @click="themeStore.setDark" @keyup.enter="themeStore.setDark"/>
-        </div>
-        <span tabindex="0" class="text-xs italic block cursor-pointer" @click="themeStore.toggleSystem" @keyup.enter="themeStore.toggleSystem">
-            (<span v-if="themeStore.system">using</span><span v-else>overriding</span> your system theme)
-        </span>
+        <button
+            type="button"
+            :aria-readonly="themeStore.system"
+            :title="`Current theme: ${themeStore.current} ${themeStore.system ? '(system theming)' : ''}`"
+            :tabindex="themeStore.system ? '-1' : '0'"
+            :class="`size-6 inline-block ${themeStore.system ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`"
+        >
+            <MoonIcon
+                v-if="themeStore.current === 'macchiato'"
+                @click="themeStore.setLight"
+                @keyup.enter="themeStore.setLight"
+            />
+            <SunIcon
+                v-else-if="themeStore.current === 'latte'"
+                @click="themeStore.setDark"
+                @keyup.enter="themeStore.setDark"
+            />
+        </button>
+        <button
+            type="button"
+            tabindex="0"
+            class="text-xs italic block cursor-pointer"
+            @click="themeStore.toggleSystem"
+            @keyup.enter="themeStore.toggleSystem"
+        >
+            (<span v-if="themeStore.system">using</span
+            ><span v-else>overriding</span>
+            your system theme)
+        </button>
     </div>
 </template>
 
 <script setup>
-import { useThemeSelector } from '@/store'
-import { MoonIcon, SunIcon } from '@heroicons/vue/16/solid'
+import { useThemeSelector } from '@/store';
+import { MoonIcon, SunIcon } from '@heroicons/vue/16/solid';
 
-const themeStore = useThemeSelector()
+const themeStore = useThemeSelector();
 </script>
